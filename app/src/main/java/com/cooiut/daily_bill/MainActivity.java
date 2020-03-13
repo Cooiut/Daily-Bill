3e5858638129caec30381f01899e594f85b419cd
zhaobochen1999@gmail.com <Cooiut11980107>
2020-03-12 23:16:02 -0700

    Add google auth
    Signed-off-by: zhaobochen1999@gmail.com <Cooiut11980107>

--- app/src/main/java/com/cooiut/daily_bill/MainActivity.java
+++ app/src/main/java/com/cooiut/daily_bill/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // Setup Database
         DatabaseReference myRefSpend = FirebaseDatabase.getInstance().getReference("spend");
         DatabaseReference myRefIncome = FirebaseDatabase.getInstance().getReference("income");
