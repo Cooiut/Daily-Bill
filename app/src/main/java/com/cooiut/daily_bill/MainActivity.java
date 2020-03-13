2ab559f20da8e364f1c488149b917631d540b8d1
Haphaistos2333 <Haphaistos2333@hotmail.com>
2020-03-13 07:37:41 -0700

    added login activity

--- app/src/main/java/com/cooiut/daily_bill/MainActivity.java
+++ app/src/main/java/com/cooiut/daily_bill/MainActivity.java
@@ -42,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
 
         // Setup Database
         DatabaseReference myRefSpend = FirebaseDatabase.getInstance().getReference("spend");
