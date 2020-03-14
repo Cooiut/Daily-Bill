bb9921305ceba211c46f7fd02f232bbbc747300d
Haphaistos2333 <Haphaistos2333@hotmail.com>
2020-03-13 19:06:49 -0700

    add analysis func

--- app/src/main/java/com/cooiut/daily_bill/Analysis.java
+++ app/src/main/java/com/cooiut/daily_bill/Analysis.java
@@ -11,8 +11,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Analysis extends AppCompatActivity {
+
+    private ArrayList<Bills> spend, income;
+
+    private class Date {
+        private int month;
+        private int year;
+
+        public Date(Bills b) {
+            this.month = b.getMonth();
+            this.year = b.getYear();
+        }
+
+        @Override
+        public String toString() {
+            return "" + month + "-" + year;
+        }
+
+        @Override
+        public boolean equals(Object d) {
+            return getClass() == d.getClass() && month == ((Date) d).month && year == ((Date) d).year;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +50,52 @@ public class Analysis extends AppCompatActivity {
         setSupportActionBar(toolbarAnalysis);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            income = extras.getParcelableArrayList("income");
+            spend = extras.getParcelableArrayList("spend");
+        }
+
+        ArrayList<String> monthYearList = getMonthYearList();
+
+        Log.d("MonthYearList", monthYearList.toString());
+    }
+
+    private ArrayList<String> getMonthYearList() {
+        ArrayList<Bills> allTransactions = new ArrayList<>(spend);
+        allTransactions.addAll(income);
+        Collections.sort(allTransactions, new Comparator<Bills>() {
+            @Override
+            public int compare(Bills o1, Bills o2) {
+                if (o1.getYear() < o2.getYear())
+                    return -1;
+                else if (o1.getYear() > o2.getYear())
+                    return 1;
+                else {
+                    if (o1.getMonth() < o2.getMonth())
+                        return -1;
+                    else if (o1.getMonth() > o2.getMonth())
+                        return 1;
+                    else {
+                        return Integer.compare(o1.getDay(), o2.getDay());
+                    }
+                }
+            }
+        });
+
+        ArrayList<Date> uniqueMonthYear = new ArrayList<>();
+        for (Bills b : allTransactions) {
+            if (!uniqueMonthYear.contains(new Date(b))) {
+                uniqueMonthYear.add(new Date(b));
+            }
+        }
+
+        ArrayList<String> res = new ArrayList<>();
+        for (Date d : uniqueMonthYear) {
+            res.add(d.toString());
+        }
+
+        return res;
     }
 }
